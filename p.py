45529c26b1b923e861f1a9622e494ab59d8cafab
Yash Dhavde <yashdhavde1511@gmail.com>
2024-04-22 21:31:41 +0530

    work in progress

--- p.py
+++ p.py
@@ -75,7 +75,7 @@ class HTMLViewerApp(QMainWindow):
 if __name__ == '__main__':
     # Specify the new path to the HTML file
     # YASH
-    html_file_path = 'D:\\Quarantine\\Projects\\Miniporject-Personal-Assistant\\jarvis.html'
+    html_file_path = 'D:\\Quarantine\\Projects\\jarvis2.0\\jarvis.html'
     # VARAD
     # html_file_path = 'C:\\Users\\varad\\OneDrive\\Desktop\\personal\\project\\jarvis2.0\\jarvis.html'
 
